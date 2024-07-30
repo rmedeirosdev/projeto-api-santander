a11a55d2a55a994c510ccd34f60656e59aa57790
rapha <raphaelmedeiros2004@gmail.com>
2024-07-29 21:43:10 -0300

    Initial commit

---
+++ src/main/java/one/digitalinnovation/projeto_api_santander/ProjetoApiSantanderApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.projeto_api_santander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoApiSantanderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoApiSantanderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/digitalinnovation/projeto_api_santander/ProjetoApiSantanderApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.projeto_api_santander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoApiSantanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
